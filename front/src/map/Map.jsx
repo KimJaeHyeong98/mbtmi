17a319c81d633de50548b6e0b7947ddf503700e5
kwak <kmj5587482@naver.com>
2025-09-22 11:37:13 +0900

    [fix] map load bug fixed.

--- front/src/map/Map.jsx
+++ front/src/map/Map.jsx
@@ -1,14 +1,13 @@
 import BottomNav from "../globaltool/BottomNav";
 import Container from "../globaltool/Container";
-import KakaoMap from "./KakaoMap";
 import MapSearch from "./MapSearch";
 
 const Map = () => {
-    return (
-        <Container>
-            <MapSearch />
-            <BottomNav />
-        </Container>
-    );
+  return (
+    <Container>
+      <MapSearch />
+      <BottomNav />
+    </Container>
+  );
 };
 export default Map;
